f8cb5f5d8fd78ad35aa06936b1dd734a936864cc
Cyrus <sgktost1@gmail.com>
2023-05-27 19:09:10 +0500

    Float extension. Transform angle to cardinal direction

---
+++ game/Extensions/FloatExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace game;
+
+internal static class FloatExtension
+{
+    private static readonly List<(float, string)> directions = new()
+    {
+        (0 * MathF.PI / 4, "N"),
+        (1 * MathF.PI / 4, "NE"),
+        (2 * MathF.PI / 4, "E"),
+        (3 * MathF.PI / 4, "SE"),
+        (4 * MathF.PI / 4, "S"),
+        (5 * MathF.PI / 4, "SW"),
+        (6 * MathF.PI / 4, "W"),
+        (7 * MathF.PI / 4, "NW"),
+    };
+
+    public static string ToCardinalDirection(this float angle)
+    {
+        foreach (var (value, direction) in directions)
+        {
+            if (angle.InBounds(value - MathF.PI / 8, value + MathF.PI / 8))
+                return direction;
+        }
+        return null;
+    }
+
+    public static bool InBounds(this float value, float minBound, float maxBound)
+    {
+        return minBound <= value && value < maxBound;
+    }
+}
