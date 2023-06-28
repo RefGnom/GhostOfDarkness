e9ae538ebff35680d22b265e57b8afb9e6e8feeb
Cyrus <sgktost1@gmail.com>
2023-06-29 01:05:52 +0500

    Created MapEditor and MapLibrary projects

---
+++ GhostOfDarkness/MapEditor/View.cs
@@ -0,0 +1,15 @@
+namespace MapEditor;
+
+public class View : Form
+{
+    public View()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        ClientSize = new Size(1280, 720);
+        Name = "MapEditor";
+    }
+}
