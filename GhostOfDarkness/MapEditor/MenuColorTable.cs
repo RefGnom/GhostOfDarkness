89fb4c80ac62c69d125628842b5eae6d30f9fd37
Cyrus <sgktost1@gmail.com>
2023-07-01 03:07:45 +0500

    Started create menu in map editor. Create map and open map

---
+++ GhostOfDarkness/MapEditor/MenuColorTable.cs
@@ -0,0 +1,36 @@
+﻿namespace MapEditor;
+
+internal class MenuColorTable : ProfessionalColorTable
+{
+    public static readonly Color Text = Color.FromArgb(220, 220, 220);
+
+    private readonly Color back = Color.FromArgb(60, 60, 60);
+    private readonly Color selected = Color.FromArgb(80, 80, 80);
+    private readonly Color border = Color.FromArgb(110, 110, 110);
+
+    public override Color MenuItemSelected => selected;
+
+    public override Color ToolStripDropDownBackground => back;
+
+    public override Color ImageMarginGradientBegin => back;
+
+    public override Color ImageMarginGradientEnd => back;
+
+    public override Color ImageMarginGradientMiddle => back;
+
+    public override Color MenuItemSelectedGradientBegin => selected;
+
+    public override Color MenuItemSelectedGradientEnd => selected;
+
+    public override Color MenuItemPressedGradientBegin => selected;
+
+    public override Color MenuItemPressedGradientMiddle => selected;
+
+    public override Color MenuItemPressedGradientEnd => selected;
+
+    public override Color MenuItemBorder => border;
+
+    public override Color MenuStripGradientBegin => back;
+
+    public override Color MenuStripGradientEnd => back;
+}
--- GhostOfDarkness/MapEditor/View.cs
+++ GhostOfDarkness/MapEditor/View.cs
@@ -2,14 +2,72 @@ namespace MapEditor;
 
 public class View : Form
 {
+    private MenuStrip menu = new();
+
     public View()
     {
         InitializeComponent();
+        InitializeWindow();
+        InitializeMenu();
     }
 
     private void InitializeComponent()
     {
+    }
+
+    private void InitializeWindow()
+    {
         ClientSize = new Size(1280, 720);
         Name = "MapEditor";
+    }
+
+    private void InitializeMenu()
+    {
+        #region menu
+        var file = new ToolStripMenuItem()
+        {
+            Text = "Файл",
+        };
+        var create = new ToolStripMenuItem()
+        {
+            Text = "Создать",
+        };
+        create.Click += CreateFile;
+        file.DropDownItems.Add(create);
+        var open = new ToolStripMenuItem()
+        {
+            Text = "Открыть",
+        };
+        open.Click += OpenFile;
+        file.DropDownItems.Add(open);
+        menu.Items.Add(file);
+
+        menu.Renderer = new ToolStripProfessionalRenderer(new MenuColorTable());
+        MainMenuStrip = menu;
+        foreach (ToolStripMenuItem item in menu.Items)
+        {
+            SetTextColor(item);
+        }
+        Controls.Add(menu);
+        #endregion
+    }
+
+    private void SetTextColor(ToolStripMenuItem item)
+    {
+        item.ForeColor = MenuColorTable.Text;
+        foreach (ToolStripMenuItem it in item.DropDownItems)
+        {
+            SetTextColor(it);
+        }
+    }
+
+    private void CreateFile(object? sender, EventArgs e)
+    {
+
+    }
+
+    private void OpenFile(object? sender, EventArgs e)
+    {
+
     }
 }
