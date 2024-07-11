07818f37902808c8fbd0f14468e232caf92a91e4
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-07-11 22:54:13 +0500

    sticky cursor

---
+++ GhostOfDarkness/Game/Controllers/StickyCursor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Game.Controllers;
+
+public class StickyCursor
+{
+    private Rectangle bounds;
+
+    public bool IsStuck { get; private set; }
+    public bool Selected { get; private set; }
+
+    public StickyCursor(Rectangle bounds)
+    {
+        this.bounds = bounds;
+    }
+
+    public void SetBounds(Rectangle newBounds)
+    {
+        bounds = newBounds;
+    }
+
+    public void Update(float scale)
+    {
+        if (MouseController.LeftButtonClicked() && MouseInBounds())
+        {
+            IsStuck = true;
+        }
+
+        if (MouseController.LeftButtonReleased())
+        {
+            IsStuck = false;
+        }
+
+        Selected = MouseInBounds() || IsStuck;
+    }
+    private bool MouseInBounds() => bounds.Contains(MouseController.WindowPosition);
+}
