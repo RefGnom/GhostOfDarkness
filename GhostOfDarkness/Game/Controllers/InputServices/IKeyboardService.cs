0465d7b0881f5fda1fef3d2d8db40759cf2421ca
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-07-12 12:34:40 +0500

    keyboard service

---
+++ GhostOfDarkness/Game/Controllers/InputServices/IKeyboardService.cs
@@ -0,0 +1,19 @@
+﻿using Core.DependencyInjection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using IUpdateable = Game.Interfaces.IUpdateable;
+
+namespace Game.Controllers.InputServices;
+
+
+[DiUsage]
+public interface IKeyboardService : IUpdateable
+{
+    void SetGameWindow(GameWindow gameWindow);
+    GameWindow GetGameWindow();
+    bool IsKeyDown(Keys key);
+    bool IsKeysDown(params Keys[] keys);
+    bool IsSingleKeyDown(Keys key);
+    bool IsSingleKeyUp(Keys key);
+    Keys[] GetPressedKeys();
+}
