7827158483454bf3bcc0292564179eebfb26b8b3
Cyrus <88629892+RefGnom@users.noreply.github.com>
2024-07-12 12:36:12 +0500

    static input. maybe temporary

---
+++ GhostOfDarkness/Game/Controllers/InputServices/Input.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Game.Controllers.InputServices;
+
+public static class Input
+{
+    private static IMouseService mouseService;
+    private static IKeyboardService keyboardService;
+
+    public static IMouseService MouseService
+    {
+        get
+        {
+            if (mouseService is null)
+            {
+                throw new InvalidOperationException("Mouse service must be initialized");
+            }
+
+            return mouseService;
+        }
+        set
+        {
+            if (mouseService is not null)
+            {
+                throw new InvalidOperationException("Mouse service must be initialized once");
+            }
+
+            mouseService = value;
+        }
+    }
+
+    public static IKeyboardService KeyboardService
+    {
+        get
+        {
+            if (keyboardService is null)
+            {
+                throw new InvalidOperationException("Keyboard service must be initialized");
+            }
+
+            return keyboardService;
+        }
+        set
+        {
+            if (keyboardService is not null)
+            {
+                throw new InvalidOperationException("Keyboard service must be initialized once");
+            }
+
+            keyboardService = value;
+        }
+    }
+}
